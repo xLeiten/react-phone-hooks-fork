31cf11b3378ceac601096aa0614974c9afe2b75a
xLeiten <oflisetrix@gmail.com>
2024-07-17 11:35:02 +0700

    fix countries and add module files

---
+++ dist/types.d.ts
@@ -0,0 +1,17 @@
+export interface PhoneNumber {
+    countryCode?: number | null;
+    areaCode?: string | null;
+    phoneNumber?: string | null;
+    isoCode?: string;
+    valid?(strict?: boolean): boolean;
+}
+export interface usePhoneOptions {
+    query?: string;
+    country?: string;
+    countryCode?: string;
+    onlyCountries?: string[];
+    excludeCountries?: string[];
+    preferredCountries?: string[];
+    disableParentheses?: boolean;
+    initialValue?: PhoneNumber | string;
+}
